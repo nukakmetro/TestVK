45da2617f4a3ab8c6218ac844230b1f010442543
Valeriy Khalin <v.khalin@vk.team>
2025-02-23 12:45:13 +0300

    Update ReviewCell

--- Test/Screens/Reviews/Cells/ReviewCell.swift
+++ Test/Screens/Reviews/Cells/ReviewCell.swift
@@ -64,13 +64,13 @@ final class ReviewCell: UITableViewCell {
     fileprivate let createdLabel = UILabel()
     fileprivate let showMoreButton = UIButton()
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupCell()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 
     override func layoutSubviews() {
